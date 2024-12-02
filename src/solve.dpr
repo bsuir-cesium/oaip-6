250865049e395115248e3a843304806ae5929c9a
MarkCesium <cesiummark01@gmail.com>
2024-12-02 19:16:14 +0300

    en -> ru, fixes, edit input method

--- src/solve.dpr
+++ src/solve.dpr
@@ -9,10 +9,17 @@ type
   TSet = set of 1 .. n;
 
 var
-  X: TMatrix;
-  i, j, k: Integer;
+  X: TMatrix = (
+    (1, 1, 1, 1, 1),
+    (1, 1, 1, 1, 2),
+    (1, 1, 1, 1, 3),
+    (1, 1, 1, 1, 4),
+    (1, 1, 1, 1, 5)
+  );
+  i, j: Integer;
   similarCount: Integer;
   doneLines: TMAS;
+  lineInArray, similar: Boolean;
 
 function InArray(var arr: TMAS; const el: Integer): Boolean;
 var
@@ -32,7 +39,8 @@ begin
   InArray := isTrue;
 end;
 
-function AreRowsSimilar(var matrix: TMatrix; const row1, row2: Integer): Boolean;
+function AreRowsSimilar(var matrix: TMatrix; const row1, row2: Integer)
+  : Boolean;
 var
   set1, set2: TSet;
   i: Integer;
@@ -50,39 +58,34 @@ begin
 end;
 
 begin
-  // Ввод матрицы
-  for i := 1 to n do
-  begin
-    doneLines[i] := 0;
-    for j := 1 to n do
-    begin
-      Write('Enter element X[', i, ',', j, ']: ');
-      ReadLn(X[i, j]);
-    end;
-  end;
-
+  // Подсчёт похожих строк
   similarCount := 0;
   for i := 1 to n - 1 do
-    if InArray(doneLines, i) then
+  begin
+    lineInArray := InArray(doneLines, i);
+    if lineInArray then
     begin
 
     end
     else
     begin
       for j := i + 1 to n do
-        if AreRowsSimilar(X, i, j) then
+      begin
+        similar := AreRowsSimilar(X, i, j);
+        if similar then
         begin
           Inc(similarCount);
           doneLines[i] := i;
-          doneLines[j] := j;
-          WriteLn('Rows ', i, ' and ', j, ' are similar.');
+          WriteLn('Строки ', i, ' и ', j, ' похожи');
         end;
+      end;
     end;
+  end;
 
   if similarCount = 0 then
-    WriteLn('No similar rows found.')
+    WriteLn('Не нашлось похожих строк')
   else
-    WriteLn('Total similar rows pairs: ', similarCount);
+    WriteLn('Итоговое кол-во похожих пар строк: ', similarCount);
   ReadLn;
 
 end.
